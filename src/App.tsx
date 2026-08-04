ab3598676a500ba975d8419890f451be2d72b775
aether <aether666@protonmail.com>
2026-08-05 01:28:31 +0200

    add react +  tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
+          <h1 className="text-3xl font-bold text-blue-600">Hello</h1>
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
